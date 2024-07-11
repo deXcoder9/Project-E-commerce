54630e7611571277381bed715aaeb7a552d33b8c
deXcoder9_ <dhrubothejuniorprogrammer@gmail.com>
2024-07-11 23:40:09 +0600

    react template implemented

---
+++ client-site/src/pages/home/home/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../../../shared/navbar/Navbar";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Home;
